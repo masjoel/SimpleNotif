1e1651acd332694eb95e2689e325918bff263fbb
masjoel <masjoel@gmail.com>
2020-11-10 08:38:52 +0700

    Simple Notification

---
+++ app/src/main/java/com/masjoel/simplenotif/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.masjoel.simplenotif
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val NOTIFICATION_ID = 1
+        var CHANNEL_ID = "channel_01"
+        var CHANNEL_NAME: CharSequence = "dicoding channel"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_24))
+                .setContentTitle(resources.getString(R.string.content_title))
+                .setContentText(resources.getString(R.string.content_text))
+                .setSubText(resources.getString(R.string.subtext))
+                .setAutoCancel(true)
+
+        /*
+        Untuk android Oreo ke atas perlu menambahkan notification channel
+        */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = CHANNEL_NAME as String?
+            mBuilder.setChannelId(CHANNEL_ID)
+            mNotificationManager.createNotificationChannel(channel)
+        }
+
+        val notification = mBuilder.build()
+
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+}
